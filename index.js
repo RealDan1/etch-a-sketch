ed7cea138ca38a148cf0c53d9d7d1d9b5b699a46
Realdan1 <69673349+RealDan1@users.noreply.github.com>
2024-06-27 15:13:28 +0200

    feat: start process of nested for loop to add hieght divs

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
-let container = document.querySelector('.container');
+let bigbox = document.querySelector('.bigbox');
 
 for (let i = 1; i <= 16; i++) {
-  let div = document.createElement('div');
-  div.id = 'div' + i;
-  div.style.border = '1px solid black';
-  div.style.height = '30px';
-  div.style.width = '30px';
-  container.appendChild(div);
+  let heightbox = document.createElement('div');
+
+  heightbox.id = 'cont' + i;
+  // container.style.display = flex;
+  for (let i = 1; i <= 16; i++) {
+    let div = document.createElement('div');
+    div.id = 'div' + i;
+    div.style.border = '1px solid black';
+    div.style.height = '30px';
+    div.style.width = '30px';
+    container.appendChild(div);
+  }
+  let br = document.createElement('br');
+  container.appendChild(br);
 }
+
 //width
 // let br = document.createElement('br');
 // container.appendChild(br);
