94627e6807b9f3bf005a617bf3bb8f0030ac04cd
Realdan1 <69673349+RealDan1@users.noreply.github.com>
2024-06-27 16:07:42 +0200

    refactor: WIP start identifying x axis

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ for (let i = 1; i <= 16; i++) {
 
   for (let j = 1; j <= 16; j++) {
     let div = document.createElement('div');
-    div.id = 'div' + j;
+    div.id = 'div' + j + '-' + i;
     div.style.border = '1px solid black';
     div.style.height = '30px';
     div.style.width = '30px';
