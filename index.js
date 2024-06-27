e45b3218fcc13fc9a503ebbaa2d331735a1c102c
Realdan1 <69673349+RealDan1@users.noreply.github.com>
2024-06-27 15:52:12 +0200

    refactor: change inner for loop to j

--- index.js
+++ index.js
@@ -7,20 +7,14 @@ for (let i = 1; i <= 16; i++) {
   bigbox.appendChild(height);
 
   // container.style.display = flex;
-  for (let i = 1; i <= 16; i++) {
+  for (let j = 1; j <= 16; j++) {
     let div = document.createElement('div');
-    div.id = 'div' + i;
+    div.id = 'div' + j;
     div.style.border = '1px solid black';
     div.style.height = '30px';
     div.style.width = '30px';
     height.appendChild(div);
   }
-  // let br = document.createElement('br');
-  // container.appendChild(br);
 }
-
-//width
-// let br = document.createElement('br');
-// container.appendChild(br);
-let height1 = document.querySelector('.height1');
-height1.style.border = '1px solid red';
+// let height1 = document.querySelector('.div1');
+// height1.style.border = '1px solid red';
