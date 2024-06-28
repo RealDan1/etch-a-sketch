b447b2289194907fc9074458e3059163278e02aa
Realdan1 <69673349+RealDan1@users.noreply.github.com>
2024-06-28 15:57:46 +0200

    refactor: swap width and height variables (now correct)

--- index.js
+++ index.js
@@ -1,25 +1,23 @@
 let bigbox = document.querySelector('.bigbox');
 
-// let heighty = [];
-
 //width
 for (let i = 1; i <= 16; i++) {
-  let height = document.createElement('div');
-  // heighty.push(height);
+  let width = document.createElement('div');
+  // widthy.push(width);
 
-  height.id = 'height' + i;
-  bigbox.appendChild(height);
+  width.id = 'width' + i;
+  bigbox.appendChild(width);
 
   //height
   for (let j = 1; j <= 16; j++) {
     let div = document.createElement('div');
-    div.id = 'div' + j + '-' + i;
+    div.id = 'div' + j;
     div.style.border = '1px solid black';
     div.style.height = '30px';
     div.style.width = '30px';
-    height.appendChild(div);
+    width.appendChild(div);
   }
 }
-// let height = document.querySelector('.div1');
-height3.style.border = '4px solid red';
-// div1.style.border = '4px solid red';
+// let width = document.querySelector('.div1');
+width3.style.border = '4px solid red';
+div3.style.border = '4px solid red';
