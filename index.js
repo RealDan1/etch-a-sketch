4be10e6d2a5755e418165a295ea74e46d72a2628
Realdan1 <69673349+RealDan1@users.noreply.github.com>
2024-06-30 11:39:33 +0200

    refactor: change boxes ID

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 let bigbox = document.querySelector('.bigbox');
-let height = [];
+let boxes = [];
 //width
 for (let i = 1; i <= 16; i++) {
   let width = document.createElement('div');
@@ -12,14 +12,16 @@ for (let i = 1; i <= 16; i++) {
   for (let j = 1; j <= 16; j++) {
     let div = document.createElement('div');
     div.id = 'div' + j;
-    height.push(div);
+    boxes.push(div);
     div.style.border = '1px solid black';
     div.style.height = '30px';
     div.style.width = '30px';
     width.appendChild(div);
   }
 }
-// let width = document.querySelector('.div1');
-width1.style.border = '4px solid red';
-height[100].style.border = '5px solid red';
-height[101].style.border = '5px solid red';
+
+// width1.style.border = '4px solid red';
+// boxes[100].style.border = '5px solid red';
+// boxes[101].style.border = '5px solid red';
+
+//change color on hover
