abbaba87d24b6ff02a59089aff87c46cb0597d09
Realdan1 <69673349+RealDan1@users.noreply.github.com>
2024-07-03 15:23:51 +0200

    feat: WIP add resize feature, but with bug
    bug: Grid resizes are now additive and don't clear the old grid.

--- index.js
+++ index.js
@@ -5,43 +5,65 @@ let boxes = [];
 let sizeButton = document.querySelector('.size_button');
 sizeButton.addEventListener('click', () => {
   let boxSize = prompt('how many boxes do you want for the grid size?');
+  if (width) {
+    for (let i = 1; i <= boxSize; i++) {
+      bigbox.removeChild(width);
+    }
+    // if (div) {
+    //   width
+  }
+  for (let i = 1; i <= boxSize; i++) {
+    let width = document.createElement('div');
+    width.id = 'width' + i;
+    bigbox.appendChild(width);
+
+    width.style.display = 'flex';
+    width.style.flex = '1';
+    width.style.flexDirection = 'column';
+
+    //height
+    for (let j = 1; j <= boxSize; j++) {
+      let div = document.createElement('div');
+      width.appendChild(div);
+      div.id = 'div' + j;
+      boxes.push(div);
+
+      div.style.display = 'flex';
+      div.style.flex = '1';
+      div.style.border = '1px solid black';
+      div.style.width = 'auto';
+    }
+
+    //change color on hover
+    boxes.forEach((element) => {
+      element.addEventListener('mouseover', () => {
+        element.style.backgroundColor = 'black';
+      });
+    });
+
+    //reset button
+    let reset_button = document.querySelector('.reset_button');
+    reset_button.addEventListener('click', () => {
+      boxes.forEach((element) => {
+        element.style.backgroundColor = 'white';
+      });
+    });
+  }
 });
 
 //width
-for (let i = 1; i <= boxSize; i++) {
-  let width = document.createElement('div');
-  width.id = 'width' + i;
-  bigbox.appendChild(width);
 
-  width.style.display = 'flex';
-  width.style.flex = '1';
-  width.style.flexDirection = 'column';
+// //change color on hover
+// boxes.forEach((element) => {
+//   element.addEventListener('mouseover', () => {
+//     element.style.backgroundColor = 'black';
+//   });
+// });
 
-  //height
-  for (let j = 1; j <= boxSize; j++) {
-    let div = document.createElement('div');
-    width.appendChild(div);
-    div.id = 'div' + j;
-    boxes.push(div);
-
-    div.style.display = 'flex';
-    div.style.flex = '1';
-    div.style.border = '1px solid black';
-    div.style.width = 'auto';
-  }
-}
-
-//change color on hover
-boxes.forEach((element) => {
-  element.addEventListener('mouseover', () => {
-    element.style.backgroundColor = 'black';
-  });
-});
-
-//reset button
-let reset_button = document.querySelector('.reset_button');
-reset_button.addEventListener('click', () => {
-  boxes.forEach((element) => {
-    element.style.backgroundColor = 'white';
-  });
-});
+// //reset button
+// let reset_button = document.querySelector('.reset_button');
+// reset_button.addEventListener('click', () => {
+//   boxes.forEach((element) => {
+//     element.style.backgroundColor = 'white';
+//   });
+// });
